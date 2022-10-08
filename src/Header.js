746410e37d7e54dd5534464f00b90eb67ea9f249
Gavin Cui <clarkhillm@gmail.com>
2022-10-09 00:01:14 +0800

    update

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,34 @@
-import React, { useEffect } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { InputNumber } from "primereact/inputnumber";
 import { Menubar } from "primereact/menubar";
 import { Tag } from "primereact/tag";
-import { Button } from "primereact/button";
+import React, { useEffect, useState, useRef } from "react";
 import logo from "../public/logo192.png";
+import { Toast } from "primereact/toast";
 
 export default function Header(props) {
-  useEffect(() => {}, []);
+  const [showParamDialog, setShowParamDialog] = useState(false);
+  const [showAccount, setShowAccount] = useState(50);
+  const [zeroAccount, setZeroAccount] = useState(3);
+  const [thresholdX, setThresholdX] = useState(0.12);
+
+  const getParam = async () => {
+    let response = await fetch("/imu/params", {
+      method: "GET",
+    });
+    let params = await response.json();
+    setZeroAccount(params.zeroAccount);
+    props.setZeroAccount(params.zeroAccount);
+    setThresholdX(params.thresholdX);
+  };
+
+  useEffect(() => {
+    getParam();
+  }, []);
+
+  const toast = useRef(null);
+
   const start = (
     <div>
       <Tag style={{ background: "#eff3f8" }}>
@@ -27,18 +50,104 @@ export default function Header(props) {
   const items = [];
   return (
     <div>
+      <Toast ref={toast} />
       <Menubar
         model={items}
         start={start}
         end={
           <Button
-            label="清除数据"
-            icon="pi pi-power-off"
-            onClick={props.clean}
+            label="参数配置"
+            icon="pi pi-chart-bar"
+            onClick={() => {
+              setShowParamDialog(true);
+            }}
           />
         }
         className="border-noround w-full shadow-2"
       />
+      <Dialog
+        header="参数配置"
+        visible={showParamDialog}
+        position="top"
+        onHide={() => {
+          setShowParamDialog(false);
+        }}
+        footer={
+          <div>
+            <Button
+              label="取消"
+              icon="pi pi-times"
+              onClick={() => {
+                setShowParamDialog(false);
+              }}
+              className="p-button-text"
+            />
+            <Button
+              label="确定"
+              icon="pi pi-check"
+              onClick={async () => {
+                props.setDataAccount(showAccount);
+
+                let rs = await fetch("/imu/params", {
+                  method: "PUT",
+                  body: JSON.stringify({
+                    thresholdX: thresholdX,
+                    zeroAccount: zeroAccount,
+                  }),
+                });
+
+                if (rs.status == 202) {
+                  toast.current.show({
+                    severity: "success",
+                    summary: "成功",
+                    detail: "参数配置成功",
+                    life: 3000,
+                  });
+                }
+
+                setShowParamDialog(false);
+              }}
+              autoFocus
+            />
+          </div>
+        }
+      >
+        <div className="card">
+          <div className="p-inputgroup field">
+            <span className="p-inputgroup-addon">保留数据</span>
+            <InputNumber
+              min={10}
+              max={200}
+              value={showAccount}
+              onValueChange={(e) => {
+                setShowAccount(e.value);
+              }}
+            />
+          </div>
+          <div className="p-inputgroup field">
+            <span className="p-inputgroup-addon">归零区间</span>
+            <InputNumber
+              min={0.01}
+              max={5}
+              value={thresholdX}
+              onValueChange={(e) => {
+                setThresholdX(e.value);
+              }}
+            />
+          </div>
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">周期指示</span>
+            <InputNumber
+              min={1}
+              max={10}
+              value={zeroAccount}
+              onValueChange={(e) => {
+                setZeroAccount(e.value);
+              }}
+            />
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 }
--- src/MainFace.js
+++ src/MainFace.js
@@ -1,15 +1,29 @@
 import _ from "lodash";
 import { Card } from "primereact/card";
 import { Chart } from "primereact/chart";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "./Header";
+import { InputTextarea } from "primereact/inputtextarea";
 
+var dataText = "";
 var working = [];
 var resting = [];
 var labels = [];
 var index = 0;
+
+var labels2 = [];
+var index2 = 0;
+var axset = [];
+
+var updateRate = 0;
+
 var ws = new WebSocket(`ws://${window.location.host}/imuws`);
+var ws2 = new WebSocket(`ws://${window.location.host}/imuws/origin`);
 export default function MainFace() {
+  const [dataAccount, setDataAccount] = useState(50);
+  const [zeroAccount, setZeroAccount] = useState(3);
+  const [originData, setOriginData] = useState("");
+  const textArea = useRef();
   useEffect(() => {}, []);
   const basicOptions = {
     animation: false,
@@ -38,13 +52,51 @@ export default function MainFace() {
         grid: {
           color: "#ebedef",
         },
-        suggestedMin: 0,
-        suggestedMax: 30,
       },
     },
   };
+
   ws.onopen = (e) => {
     console.log("ws opened...");
+  };
+
+  ws2.onopen = (e) => {
+    console.log("ws2 opened...");
+  };
+  ws2.onmessage = (e) => {
+    updateRate += 1;
+    let data = JSON.parse(e.data).value;
+    // console.log(data);
+    dataText += data;
+    dataText += "\r\n";
+
+    const area = textArea.current;
+    area.scrollTop = area.scrollHeight;
+
+    labels2.push(index2);
+    axset.push(data);
+
+    if (index2 > 100) {
+      axset.shift();
+      labels2.shift();
+    }
+
+    if (updateRate == 10) {
+      setOriginData(dataText);
+      setOriginChartData({
+        labels: labels2,
+        datasets: [
+          {
+            label: "加速度",
+            backgroundColor: "#42A5F5",
+            data: axset,
+          },
+        ],
+      });
+      updateRate = 0;
+    }
+
+    index2 += 1;
   };
 
   const [basicData, setBasicData] = useState({
@@ -58,6 +110,17 @@ export default function MainFace() {
       {
         label: "休息",
         backgroundColor: "#FFA726",
+        data: [],
+      },
+    ],
+  });
+
+  const [originChartData, setOriginChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "加速度",
+        backgroundColor: "#42A5F5",
         data: [],
       },
     ],
@@ -79,27 +142,31 @@ export default function MainFace() {
     chartData.datasets[0].data = working;
     chartData.datasets[1].data = resting;
 
+    if (index > dataAccount) {
+      working.shift();
+      resting.shift();
+      labels.shift();
+    }
+
+    setBasicData(chartData);
     index += 1;
-
-    setBasicData(chartData);
-  };
-
-  const clean = () => {
-    working = [];
-    resting = [];
-    labels = [];
-    let chartData = _.cloneDeep(basicData);
-    chartData.labels = [];
-    chartData.datasets[0].data = [];
-    chartData.datasets[1].data = [];
-    setBasicData(chartData);
   };
 
   return (
     <div>
-      <Header clean={clean} />
-      <Card title="实时数据" className="m-2">
+      <Header setDataAccount={setDataAccount} setZeroAccount={setZeroAccount} />
+      <Card title={`周期分析 (显示${dataAccount}个数据)`} className="m-2">
         <Chart type="bar" data={basicData} options={basicOptions} />
+      </Card>
+      <Card title="原始数据" className=" m-2">
+        <div className="flex card-container">
+          <div className="flex-1">
+            <InputTextarea value={originData} rows={25} ref={textArea} />
+          </div>
+          <div className="flex-auto">
+            <Chart type="bar" data={originChartData} options={basicOptions} />
+          </div>
+        </div>
       </Card>
     </div>
   );
