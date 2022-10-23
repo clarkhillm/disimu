e27f774beec5ceef6b1705c30d434b803d7a6158
Gavin Cui <clarkhillm@gmail.com>
2022-10-24 00:38:56 +0800

    update ...

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,10 @@
 import { Card } from "primereact/card";
-import { TabPanel, TabView } from "primereact/tabview";
 import React from "react";
 export default function Home() {
   return (
     <div>
-      <Card className="m-2">
-        <TabView>
-          <TabPanel header="首页"></TabPanel>
-        </TabView>
+      <Card className="m-2" title="首页">
+        这里是首页，目前还是一片空白
       </Card>
     </div>
   );
--- src/dev/DevMain.js
+++ src/dev/DevMain.js
@@ -1,11 +1,9 @@
-import { Card } from "primereact/card";
-import { TabPanel, TabView } from "primereact/tabview";
-import { Toolbar } from "primereact/toolbar";
 import { Button } from "primereact/button";
-import { DataTable } from "primereact/datatable";
+import { Card } from "primereact/card";
 import { Column } from "primereact/column";
-import React from "react";
-import { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Toolbar } from "primereact/toolbar";
+import React, { useEffect, useState } from "react";
 import { list } from "./service";
 export default function DevMain() {
   let [devList, setDevList] = useState([]);
@@ -17,59 +15,53 @@ export default function DevMain() {
     getList();
   }, []);
   return (
-    <div>
-      <Card className="m-2">
-        <TabView>
-          <TabPanel header="设备管理">
-            <Toolbar
-              className="mb-4"
-              left={
-                <div>
-                  <Button label="新建" icon="pi pi-plus" />
-                </div>
-              }
-            ></Toolbar>
-            <DataTable
-              value={devList}
-              dataKey="id"
-              paginator
-              rows={5}
-              rowsPerPageOptions={[5, 10, 25]}
-              paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-              currentPageReportTemplate=" 显示{first}到{last}条  共{totalRecords}条"
-              responsiveLayout="scroll"
-            >
-              <Column header="ID" field="devId" />
-              <Column header="端口" field="measurement" />
-              <Column header="归零" field="zeroMetric" />
-              <Column header="正阈值" field="positiveMetric" />
-              <Column header="负阈值" field="negativeMetric" />
-              <Column
-                header="操作"
-                body={
-                  <div className="">
-                    <Button
-                      label="查看15分钟数据"
-                      icon="pi pi-search"
-                      className="p-button-rounded p-button-sm mr-2"
-                    />
-                    <Button
-                      label="更新"
-                      icon="pi pi-file"
-                      className="p-button-rounded p-button-warning p-button-sm mr-2"
-                    />
-                    <Button
-                      label="删除"
-                      icon="pi pi-trash"
-                      className="p-button-rounded p-button-danger p-button-sm"
-                    />
-                  </div>
-                }
+    <Card className="m-2" title="设备管理">
+      <Toolbar
+        className="mb-4"
+        left={
+          <div>
+            <Button label="新建设备" icon="pi pi-plus" />
+          </div>
+        }
+      ></Toolbar>
+      <DataTable
+        value={devList}
+        dataKey="id"
+        paginator
+        rows={5}
+        rowsPerPageOptions={[5, 10, 25]}
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        currentPageReportTemplate=" 显示{first}到{last}条  共{totalRecords}条"
+        responsiveLayout="scroll"
+      >
+        <Column header="ID" field="devId" />
+        <Column header="端口" field="measurement" />
+        <Column header="归零" field="zeroMetric" />
+        <Column header="正阈值" field="positiveMetric" />
+        <Column header="负阈值" field="negativeMetric" />
+        <Column
+          header="操作"
+          body={
+            <div className="">
+              <Button
+                label="查看15分钟数据"
+                icon="pi pi-search"
+                className="p-button-rounded p-button-sm mr-2"
               />
-            </DataTable>
-          </TabPanel>
-        </TabView>
-      </Card>
-    </div>
+              <Button
+                label="更新"
+                icon="pi pi-file"
+                className="p-button-rounded p-button-warning p-button-sm mr-2"
+              />
+              <Button
+                label="删除"
+                icon="pi pi-trash"
+                className="p-button-rounded p-button-danger p-button-sm"
+              />
+            </div>
+          }
+        />
+      </DataTable>
+    </Card>
   );
 }
